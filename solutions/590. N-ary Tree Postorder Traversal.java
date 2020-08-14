491768e20b8cf3a240ef418ede6f82dee68b1da7
Prabhakara Reddy Gaddam <prabhakargaddam1729@gmail.com>
2020-08-14 16:04:30 -0400

    3 ms

--- solutions/590. N-ary Tree Postorder Traversal.java
+++ solutions/590. N-ary Tree Postorder Traversal.java
@@ -19,15 +19,29 @@ class Node {
 ​
 class Solution {
     public List<Integer> postorder(Node root) {
-        List<Integer> list = new ArrayList();
-        helper(root,list);
+        List<Integer> list = new LinkedList();
+        if (root == null) return list;
+        Stack<Node> stack = new Stack();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            Node temp = stack.pop();
+            list.add(0,temp.val);
+            for(var node:temp.children){
+                stack.push(node);
+            }
+        }
         return list;
     }
-    private void helper(Node root,List<Integer> list){
-        if(root==null) return;
-        for(var child:root.children){
-            helper(child,list);
-        }
-        list.add(root.val);
-    }
+    
 }
+// class Solution {
+//     public List<Integer> postorder(Node root) {
+//         List<Integer> list = new ArrayList();
+//         helper(root,list);
+//         return list;
+//     }
+//     private void helper(Node root,List<Integer> list){
+//         if(root==null) return;
+//         for(var child:root.children){
+//             helper(child,list);
+//         }
