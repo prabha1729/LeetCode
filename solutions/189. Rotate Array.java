af00a031f1c5f6923a652f271b7f233f35b6b09e
Prabhakara Reddy Gaddam <prabhakargaddam1729@gmail.com>
2020-08-14 15:07:07 -0400

    0 ms

---
+++ solutions/189. Rotate Array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void reverseArray(int[] nums,int low,int high){
+        int temp = -1 ;
+        while(low<high){
+            temp = nums[low];
+            nums[low]=nums[high];
+            nums[high] =temp;
+            low++;
+            high--;
+        }
+    }
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        k = k%len;
+        reverseArray(nums,0,len-1);
+        reverseArray(nums,0,k-1);
+        reverseArray(nums,k,len-1);
+        
+        }
+}
