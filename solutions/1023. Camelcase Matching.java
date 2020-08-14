0aa1567afe2c10b82f5a7aa53b82b5b4df77681a
Prabhakara Reddy Gaddam <prabhakargaddam1729@gmail.com>
2020-08-14 15:37:09 -0400

    0 ms

--- solutions/1023. Camelcase Matching.java
+++ solutions/1023. Camelcase Matching.java
@@ -1,24 +1,8 @@
 class Solution {
-    public List<Boolean> camelMatch(String[] queries, String pattern) {
-        List<Boolean> list = new ArrayList<>(queries.length);
-        
-        for(String query : queries) {
-            boolean match = isMatch(query.toCharArray(), pattern.toCharArray());
-            list.add(match);
-        }
-        return list;
-    }
-    
-    public boolean isMatch(char[] query, char[] pattern) {
-        int j = 0;
-        for(int i = 0 ; i < query.length ; i++) {
-            if(j < pattern.length && query[i] == pattern[j]) {
-                j++;
-            }
-            else if(Character.isUpperCase(query[i])) {
-                return false;
-            }
-        }
-        return j == pattern.length;
+  public List<Boolean> camelMatch(String[] queries, String pattern) {
+        String newPattern = "[a-z]*" + String.join("[a-z]*", pattern.split("")) + "[a-z]*";
+        List<Boolean> ans = new ArrayList<>(queries.length);
+        for (String q : queries) { ans.add(q.matches(newPattern)); }
+        return ans;
     }
 }
