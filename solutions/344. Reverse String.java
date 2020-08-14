80c63fae0c74442799901e1b6aa51d60bd29bfd9
Prabhakara Reddy Gaddam <prabhakargaddam1729@gmail.com>
2020-08-14 15:24:56 -0400

    0 ms

--- solutions/344. Reverse String.java
+++ solutions/344. Reverse String.java
@@ -1,6 +1,5 @@
 class Solution {
     public void reverseString(char[] s) {
-        // char temp ='\0';
         for(int i=0,j=s.length-1;i<j;i++,j--){
            char temp=s[i];
             s[i]=s[j];
