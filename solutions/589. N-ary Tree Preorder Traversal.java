4c5b00dff1aa2d93886b3f2747dd04bbdda4033b
Prabhakara Reddy Gaddam <prabhakargaddam1729@gmail.com>
2020-08-14 16:04:23 -0400

    2 ms

--- solutions/589. N-ary Tree Preorder Traversal.java
+++ solutions/589. N-ary Tree Preorder Traversal.java
@@ -17,17 +17,31 @@ class Node {
 };
 */
 ​
-class Solution {
-    public List<Integer> preorder(Node root) {
+class Solution{
+     public List<Integer> preorder(Node root) {
         List<Integer> list = new ArrayList();
-        helper(root,list);
-        return list;
-    }
-    private void helper(Node root,List<Integer> list){
-        if(root==null) return;
-        list.add(root.val);
-        for(var child:root.children){
-            helper(child,list);   
+        if (root==null) return list;
+         Stack<Node> stack = new Stack();
+         stack.push(root);
+         while(!stack.isEmpty()){
+             Node temp = stack.pop();
+             list.add(temp.val);
+            for (int i = temp.children.size() - 1; i >= 0; i--){
+                stack.add(temp.children.get(i));
         }
-    }
+         }
+         return list;
+     }
 }
+//Recursive
+// class Solution {
+//     public List<Integer> preorder(Node root) {
+//         List<Integer> list = new ArrayList();
+//         helper(root,list);
+//         return list;
+//     }
+//     private void helper(Node root,List<Integer> list){
+//         if(root==null) return;
+//         list.add(root.val);
+//         for(var child:root.children){
+//             helper(child,list);   
