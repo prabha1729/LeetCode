c6f22b3b0857d234a1473b27d24adc41b0b66783
Prabhakara Reddy Gaddam <prabhakargaddam1729@gmail.com>
2020-08-14 15:10:56 -0400

    1 ms

--- solutions/122. Best Time to Buy and Sell Stock II.java
+++ solutions/122. Best Time to Buy and Sell Stock II.java
@@ -1,11 +1,30 @@
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int profit=0;
+//         for(int i=0;i<prices.length-1;i++){
+//             if(prices[i]<prices[i+1]){
+//                 profit = profit+(prices[i+1]-prices[i]);
+//             }
+//         }
+//         return profit;
+//     }
+// }
+​
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit=0;
-        for(int i=0;i<prices.length-1;i++){
-            if(prices[i]<prices[i+1]){
-                profit = profit+(prices[i+1]-prices[i]);
-            }
+        int i = 0;
+        int valley = prices[0];
+        int peak = prices[0];
+        int maxprofit = 0;
+        while (i < prices.length - 1) {
+            if (i < prices.length - 1 && prices[i] >= prices[i + 1])
+                i++;
+            valley = prices[i];
+            if (i < prices.length - 1 && prices[i] <= prices[i + 1])
+                i++;
+            peak = prices[i];
+            maxprofit += peak - valley;
         }
-        return profit;
+        return maxprofit;
     }
 }
