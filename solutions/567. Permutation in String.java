caa26ce26e96f2567c950dddb75e261e7fc6f59d
Prabhakara Reddy Gaddam <prabhakargaddam1729@gmail.com>
2020-08-16 01:15:16 -0400

    5 ms

--- solutions/567. Permutation in String.java
+++ solutions/567. Permutation in String.java
@@ -1,19 +1,28 @@
 class Solution {
-    public boolean checkInclusion(String s1, String s2) {  
-      if(s1.length()>s2.length())
+    public boolean checkInclusion(String s1, String s2) {
+        int len1 = s1.length(), len2 = s2.length();
+        if (len1 > len2) return false;
+        
+        int[] count = new int[26];
+        for (int i = 0; i < len1; i++) {
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
+        if (allZero(count)) return true;
+        
+        for (int i = len1; i < len2; i++) {
+            count[s2.charAt(i) - 'a']--;
+            count[s2.charAt(i - len1) - 'a']++;
+            if (allZero(count)) return true;
+        }
+        
         return false;
-      int[] ch1=new int[26];
-      int[] ch2=new int[26];
-      for(int i=0;i<s1.length();i++){
-          ch1[s1.charAt(i)-'a']++;
-          ch2[s2.charAt(i)-'a']++;
-      }
-      for(int i=0;i<s2.length()-s1.length();i++){
-          if(Arrays.equals(ch1,ch2))
-              return true;
-          ch2[s2.charAt(i+s1.length())-'a']++;
-          ch2[s2.charAt(i)-'a']--;
-      }
-        return Arrays.equals(ch1,ch2);
+    }
+    
+    private boolean allZero(int[] count) {
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
+        }
+        return true;
     }
 }
