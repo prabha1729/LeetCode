79dd2dc9432f1a4c18e49958f4fd556d9dca75e8
Prabhakara Reddy Gaddam <prabhakargaddam1729@gmail.com>
2020-08-18 20:52:54 -0400

    4 ms

--- solutions/239. Sliding Window Maximum.java
+++ solutions/239. Sliding Window Maximum.java
@@ -1,28 +1,33 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        
-        if (nums == null || k <= 0) {
+       if (nums == null || k <= 0) {
             return new int[0];
         }
         int n = nums.length;
-        Deque<Integer> deque = new ArrayDeque<>();
-        int noOfwindows = n-k+1;
-        int[] result = new int[noOfwindows];
+        int[] result = new int[n-k+1];
         int idx = 0;
-        for(int i=0;i<n;i++){
-            int windowStart = i-k+1;
-            if(!deque.isEmpty() && deque.peekFirst()<windowStart){
-                deque.pollFirst();
+        int max = findMaxInRange(nums,0,k-1);
+        result[idx++] = max;
+        for(int i=k;i<n;i++){
+            if(max==nums[i-k]){
+                int current_max = findMaxInRange(nums,i-k+1,i);
+                max = current_max;
+                result[idx++] = max;
+            }else{
+                if(nums[i]>max){
+                    max=nums[i];
+                }
+                 result[idx++] = max;
             }
-            while(!deque.isEmpty() && nums[deque.peekLast()]<=nums[i]){
-                deque.pollLast();
-            }
-            deque.offer(i);
-            if(windowStart>=0){
-                result[idx] = nums[deque.peek()];
-                idx++;
-            }
+            
         }
         return result;
     }
-}
+    public int findMaxInRange(int[] array, int start,int end){
+        int max = Integer.MIN_VALUE;
+        for(int i=start;i<=end;i++){
+            if(array[i]>max){
+                max = array[i];
+            }
+        }
+        return max; 
