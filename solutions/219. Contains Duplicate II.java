347868652f77624aa6d90a88693a368b37b9a850
Prabhakara Reddy Gaddam <prabhakargaddam1729@gmail.com>
2020-08-23 18:48:14 -0400

    5 ms

--- solutions/219. Contains Duplicate II.java
+++ solutions/219. Contains Duplicate II.java
@@ -7,7 +7,21 @@ class Solution {
                 if (i - map.get(nums[i]) <= k) return true;
             }
             map.put(nums[i], i);
-        }
+        }
         return false;
     }
 }
+​
+/*class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) return true;
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}*/
